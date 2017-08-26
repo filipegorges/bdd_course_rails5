2ee70568846185cf966be65067f55cb51dc030ff
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2017-08-26 23:35:33 +0000

    Implement edit feature

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "Articles", type: :request do
       before { get "/articles/xxxx" }
       
       it "handles non-existing article" do
-        expect(response.status).to eq 404
+        expect(response.status).to eq 302
         flash_message = "The article you are looking for could not be found"
         expect(flash[:alert]).to eq flash_message
       end
