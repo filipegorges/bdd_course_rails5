4c3d8b6d45a786f586371e605d023b90b510645c
Filipe Gorges Reuwsaat <lipirr@hotmail.com>
2017-08-27 21:40:37 +0000

    Remove login from listing article spec

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -3,7 +3,6 @@ require "rails_helper"
 RSpec.feature "Listing articles" do
   before do
       john = User.create!(email: "john@example.com", password: "password")
-      login_as(john)
       @article1 = Article.create(title: "The first article", body: "Lorem ipsum dolor sit amet, consectetur.", user: john)
       @article2 = Article.create(title: "The second article", body: "Body of 2nd article", user: john)
   end
